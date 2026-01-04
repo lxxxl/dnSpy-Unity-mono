9bd7a090a991c09ad04ea261c73ed0fa19ef5550
lxxxl <xi_long_li@hotmail.com>
2026-01-04 18:05:55 +0800

    Delete unity-2017.1.2 directory

--- unity-2017.1.2/mono/mini/mini-windows.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MONO_MINI_WINDOWS_H__
-#define __MONO_MINI_WINDOWS_H__
-
-#include "mini.h"
-
-gboolean win32_stack_overflow_walk (StackFrameInfo *frame, MonoContext *ctx, gpointer data) MONO_INTERNAL;
-
-#endif
